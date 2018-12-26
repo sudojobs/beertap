440572f4c9cd6ae561f268f79b513520e5ef2876
sudojobs <shobjobs@gmail.com>
2018-12-26 12:13:09 +0530

    updated

--- server/WebAPI/server.py
+++ server/WebAPI/server.py
@@ -1,23 +1,39 @@
-import RPi.GPIO as GPIO
 from flask import Flask, jsonify, abort, request, render_template
 from relaydefinitions import relays, relayIdToPin
+from gpiozero import LED
+from gpiozero.pins.pigpio import PiGPIOFactory
+from time import sleep
+import paho.mqtt.client as mqtt
+MQTT_PATH   = "test_channel"
+MQTT_SERVER= "192.168.1.9" 
+
+
+factory = PiGPIOFactory(host='192.168.1.9')
+led = LED(18, pin_factory=factory)
 
 app = Flask(__name__)
 
-GPIO.setmode(GPIO.BCM)
+#GPIO.setmode(GPIO.BCM)
 
-relayStateToGPIOState = {
-    'off' : GPIO.LOW,
-    'on' : GPIO.HIGH
-    }
+#relayStateToGPIOState = {
+#    'off' : GPIO.LOW,
+#    'on' : GPIO.HIGH
+#    }
 
 def Setup():
-    for relay in relays:
-        GPIO.setup(relayIdToPin[relay['id']],GPIO.OUT)
-        GPIO.output(relayIdToPin[relay['id']],relayStateToGPIOState[relay['state']])
+    print("Setup Complete") 
+
+#for relay in relays:
+    #    GPIO.setup(relayIdToPin[relay['id']],GPIO.OUT)
+    #    GPIO.output(relayIdToPin[relay['id']],relayStateToGPIOState[relay['state']])
 
 def UpdatePinFromRelayObject(relay):
-    GPIO.output(relayIdToPin[relay['id']],relayStateToGPIOState[relay['state']])
+    print (relay)
+    if(relay['state']=='on'): 
+       led.on()
+    else:
+       led.off()
+    #GPIO.output(relayIdToPin[relay['id']],relayStateToGPIOState[relay['state']])
 
 @app.route('/WebRelay/', methods=['GET'])
 def index():
@@ -50,11 +66,36 @@ def update_relay(relay_id):
     UpdatePinFromRelayObject(relay)
     return jsonify({'relay': relay})
 
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
+  # Subscribing in on_connect() means that if we lose the connection and
+  # reconnect then subscriptions will be renewed.
+    client.subscribe(MQTT_PATH)
+
+  # The callback for when a PUBLISH message is received from the server.
+def on_message(client, userdata, msg):
+    print("%s %s" % (msg.topic,msg.payload))
+    table=msg.payload.decode('utf-8')
+    status=msg.payload.decode('utf-8')
+    print(table)
+    print(status) 
+
+
 if __name__ == "__main__":
     print("starting...")
     try:
         Setup()
+        client = mqtt.Client()
+        client.on_connect = on_connect
+        client.on_message = on_message
+        client.connect(MQTT_SERVER, 1883, 60)
+        # Blocking call that processes network traffic, dispatches callbacks and
+        # handles reconnecting.
+        # Other loop*() functions are available that give a threaded interface and a
+        # manual interface.
+        client.loop_start()
         app.run(host='0.0.0.0',port=80,debug=False)
     finally:
         print("cleaning up")
-        GPIO.cleanup()
+        #GPIO.cleanup()
+
