f766639e31d1ba725635ed0ade188ecb617b8958
sudojobs <sudojobs@gmail.com>
2018-12-27 02:58:36 +0800

    last working 80ml

--- table/table.py
+++ table/table.py
@@ -6,12 +6,18 @@ import time
 import datetime
 value =0
 
-MQTT_SERVER = "192.168.43.27"
-MQTT_PATH = "test_channel"
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(24,GPIO.OUT)
-GPIO.setup(14 , GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.add_event_detect(14, GPIO.BOTH, callback=sensorCallback, bouncetime=200)
+
+global count
+count = 0
+start_counter = 0
+flow1 = 0
+
+def countPulse(channel):
+   global count
+   if start_counter == 1:
+      count = count + 1
+      flow = count / (60 * 7.5)
+  
 
 def sensorCallback(channel):
   # Called if sensor output changes
@@ -24,12 +30,28 @@ def sensorCallback(channel):
     # Magnet
     print("Sensor LOW " + stamp)
 
+MQTT_SERVER = "192.168.43.27"
+MQTT_PATH = "test_channel"
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(24,GPIO.OUT)
+GPIO.setup(14 , GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.add_event_detect(14, GPIO.BOTH, callback=countPulse, bouncetime=200)
+
+
 while True:
  try:
      if(GPIO.input(24)):
-        publish.single(MQTT_PATH,value, hostname=MQTT_SERVER)
-        value=value+5
-        time.sleep(0.1)
+        start_counter = 1
+        time.sleep(1)
+        start_counter = 0
+        flow = (count * 16)
+        flow1=flow1 + flow
+        print (flow1)
+        count = 0
+        time.sleep(0.5)    
+        publish.single(MQTT_PATH,flow1, hostname=MQTT_SERVER)
+     else:
+        flow1 = 0
  except KeyboardInterrupt:
         # Reset GPIO settings
         GPIO.cleanup()
